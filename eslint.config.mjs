6d33a6f578b1cd2fb44581619f3171b194da4143
alic3 <alic3dev@gmail.com>
2024-11-20 16:42:27 -0500

    Add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  // ...tseslint.configs.strictTypeChecked,
+  // ...tseslint.configs.stylisticTypeChecked,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  {
+    // languageOptions: {
+    //   parserOptions: {
+    //     project: true,
+    //     projectService: true,
+    //     tsconfigRootDir: import.meta.dirname,
+    //   },
+    // },
+    ignores: ["build"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+    files: ["src/**/*.ts"],
+  }
+);
